ac110b6f56914b2128371dd1800b919bd8cd3c02
Gaurav Choudhary <gaurav.choudhary@simpplr.com>
2024-01-22 22:56:45 +0530

    Testing craft.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+// pages/index.js
 
-function App() {
+import React from 'react';
+import {Typography, Paper, Grid} from '@material-ui/core';
+
+import { Toolbox } from './Components/Toolbox';
+import { SettingsPanel } from './Components/SettingsPanel';
+import { Topbar } from './Components/Topbar';
+
+import { Container } from './Components/Container';
+import { Button } from './Components/Button';
+import { Card, CardTop, CardBottom } from './Components/Card';
+import { Text } from './Components/Text';
+import {Editor, Frame, Element} from "@craftjs/core";
+
+const json = {"ROOT":{"type":{"resolvedName":"Container"},"isCanvas":true,"props":{"padding":5,"background":"#eee"},"displayName":"Container","custom":{},"hidden":false,"nodes":["iVYrvsDgkh","l1m4eBq5lH","-yWiexDl5f","887Tk273KW"],"linkedNodes":{}},"iVYrvsDgkh":{"type":{"resolvedName":"Card"},"isCanvas":false,"props":{},"displayName":"Card","custom":{},"parent":"ROOT","hidden":false,"nodes":[],"linkedNodes":{"text":"3ZVVxkhwv2","buttons":"pvEQZgZxRX"}},"l1m4eBq5lH":{"type":{"resolvedName":"Button"},"isCanvas":false,"props":{"size":"small","variant":"outlined","children":"Click"},"displayName":"Button","custom":{},"parent":"ROOT","hidden":false,"nodes":[],"linkedNodes":{}},"-yWiexDl5f":{"type":{"resolvedName":"Text"},"isCanvas":false,"props":{"size":"small","text":"Hellllllloooooo!!!!"},"displayName":"Text","custom":{},"parent":"ROOT","hidden":false,"nodes":[],"linkedNodes":{}},"887Tk273KW":{"type":{"resolvedName":"Container"},"isCanvas":true,"props":{"padding":2,"background":"#999"},"displayName":"Container","custom":{},"parent":"ROOT","hidden":false,"nodes":["htj2AflEPX"],"linkedNodes":{}},"htj2AflEPX":{"type":{"resolvedName":"Text"},"isCanvas":false,"props":{"size":"small","text":"It's me again!"},"displayName":"Text","custom":{},"parent":"887Tk273KW","hidden":false,"nodes":[],"linkedNodes":{}},"3ZVVxkhwv2":{"type":{"resolvedName":"CardTop"},"isCanvas":true,"props":{},"displayName":"CardTop","custom":{},"parent":"iVYrvsDgkh","hidden":false,"nodes":["maiSioriGE","05ngLdIdQg"],"linkedNodes":{}},"maiSioriGE":{"type":{"resolvedName":"Text"},"isCanvas":false,"props":{"text":"Testing","fontSize":20},"displayName":"Text","custom":{},"parent":"3ZVVxkhwv2","hidden":false,"nodes":[],"linkedNodes":{}},"05ngLdIdQg":{"type":{"resolvedName":"Text"},"isCanvas":false,"props":{"text":"Testing again","fontSize":15},"displayName":"Text","custom":{},"parent":"3ZVVxkhwv2","hidden":false,"nodes":[],"linkedNodes":{}},"pvEQZgZxRX":{"type":{"resolvedName":"CardBottom"},"isCanvas":true,"props":{},"displayName":"CardBottom","custom":{},"parent":"iVYrvsDgkh","hidden":false,"nodes":["EfNjyzQ5yN"],"linkedNodes":{}},"EfNjyzQ5yN":{"type":{"resolvedName":"Button"},"isCanvas":false,"props":{"size":"small","text":"Learn more"},"displayName":"Button","custom":{},"parent":"pvEQZgZxRX","hidden":false,"nodes":[],"linkedNodes":{}}}
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{margin: "0 auto", width: "800px"}}>
+        <Editor resolver={{Card, Button, Text, Container, CardTop, CardBottom}}> 
+        <Topbar />
+          <Grid container spacing={3}>          
+            <Grid item xs>
+              <Frame json={json}>
+                {/* <Container padding={5} background="#eee"> */}
+                <Element is={Container} padding={5} background="#eee" canvas>
+                  <Card />
+                  <Button size="small" variant="outlined">Click</Button>
+                  <Text size="small" text="Hi world!" />
+                  <Element is={Container} padding={2} background="#999" canvas>
+                  {/* <Container padding={6} background="#999"> */}
+                    <Text size="small" text="It's me again!" />
+                  {/* </Container> */}
+                  </Element>
+                {/* </Container> */}
+                </Element>
+              </Frame>
+            </Grid>
+            <Grid item xs={3}>
+              <Paper>
+                  <Toolbox />
+                  <SettingsPanel />
+              </Paper>          
+            </Grid>
+          </Grid>
+        </Editor>
     </div>
   );
 }
 
-export default App;
---
+++ src/Components/Text.jsx
@@ -0,0 +1,55 @@
+// components/user/Text.js
+import React, {useState, useEffect} from "react";
+import { useNode } from "@craftjs/core";
+import ContentEditable from 'react-contenteditable'
+import {Slider, FormControl, FormLabel} from "@material-ui/core";
+
+export const Text = ({text, fontSize, textAlign}) => {
+
+    // const { connectors: {connect, drag}, actions: {setProp} } = useNode();
+    const { connectors: {connect, drag}, hasSelectedNode, hasDraggedNode, actions: {setProp} } = useNode((state) => ({
+        hasSelectedNode: state.events.selected.size > 0,
+        hasDraggedNode: state.events.dragged.size > 0
+      }));
+
+      const [editable, setEditable] = useState(false);
+
+      useEffect(() => {!hasSelectedNode && setEditable(false)}, [hasSelectedNode]);
+
+
+  return (
+      <div
+      ref={ref => connect(drag(ref))}
+      >
+         {/* <p style={{fontSize}}>{text}</p> */}
+         <ContentEditable
+         disabled={editable}
+        html={text} 
+        onChange={e => 
+          setProp(props => 
+            props.text = e.target.value.replace(/<\/?[^>]+(>|$)/g, "")  
+          )
+        } 
+        tagName="p"
+        style={{fontSize: `${fontSize}px`, textAlign}}
+      />
+      {
+        hasSelectedNode && (
+          <FormControl className="text-additional-settings" size="small">
+            <FormLabel component="legend">Font size</FormLabel>
+            <Slider
+              defaultValue={fontSize}
+              step={1}
+              min={7}
+              max={50}
+              valueLabelDisplay="auto"
+              onChange={(_, value) => {
+                setProp(props => props.fontSize = value);
+              }}
+            />
+          </FormControl>
+        )
+      }
+      </div>
+  )
+}
